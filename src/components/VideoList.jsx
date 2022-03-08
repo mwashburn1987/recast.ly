1ac61cc262829cda328ae51a2f140e750073a5d2
Michael Washburn <m.washburn@mail.com>
2022-03-07 18:27:35 -0800

    added some app functionality, working on video list

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,8 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry currentVideo={props.videoItems}/></h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
